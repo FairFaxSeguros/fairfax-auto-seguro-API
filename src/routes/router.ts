0182b23f5f395e0b710538554365d366f30d4d67
Leonardo Otero <leonardo.otero390@gmail.com>
2022-06-04 15:08:32 -0400

    fix: schema.prisma models

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-import validateToken from '../middlewares/validateToken.js';
 
 const routes = Router();
 
 routes.get('/health', async (req, res) => {
   res.sendStatus(200);
 });
-routes.use(validateToken);
 
 export default routes;
